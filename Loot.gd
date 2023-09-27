7e9149fa9e9d5141817aa59899756764799afe2f
Dmitrii Borisov <rundoom1337@gmail.com>
2023-09-27 16:19:49 +0700

    Loot and food added

---
+++ Loot.gd
@@ -0,0 +1,6 @@
+extends Loot
+class_name Food
+
+
+@export var dissolve_tick := 1
+@export var dissolve_rate := 1
---
+++ loot/loot.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+class_name Loot
+
+
+@export var volume := 1
+@export var item_name := "untitled_item"
