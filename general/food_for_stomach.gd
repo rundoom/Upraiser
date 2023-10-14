5ddcfce6f31846009e1fd5215014dcb80aa564dc
Dmitrii Borisov <rundoom1337@gmail.com>
2023-10-14 18:30:57 +0700

    Grass in stomach

--- general/food_for_stomach.gd
+++ general/food_for_stomach.gd
@@ -3,11 +3,47 @@ class_name FoodForStomach
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $SubViewportContainer/SubViewport/Sprite2D
+
+
+@export_group("image")
+@export var food_image: Texture2D:
+	set(val):
+		food_image = val
+		sprite_2d.texture = food_image
+		
+@export var viewport_scale: Vector2:
+	set(val):
+		viewport_scale = val
+		$SubViewportContainer.scale = viewport_scale
+		
+@export var food_image_region: Rect2:
+	set(val):
+		food_image_region = val
+		sprite_2d.region_rect = food_image_region
+		
+		
+@export var food_image_size: Vector2:
+	set(val):
+		food_image_size = val
+		$SubViewportContainer.size = val
+		
+@export var food_image_position: Vector2:
+	set(val):
+		food_image_position = val
+		$SubViewportContainer.position = val
+
+@export_group("", "")
 
 @onready var bubbles := $Bubbles
 @onready var food_represented: Food:
 	set(val):
 		food_represented = val
+		food_image = val.image_sprite
+		viewport_scale = val.image_region_scale
+		food_image_region = val.image_region_rect
+		food_image_size = val.image_size
+		food_image_position = (-val.image_size / 2) * val.image_region_scale
 		val.volume_changed.connect(volume_changed)
 		volume_changed(val.volume, val.MAX_VOLUME)
 
@@ -23,7 +59,7 @@ func hold_out_bubbles() -> void:
 
 func volume_changed(vol: int, max_vol: int) -> void:
 	var is_just_started = not animation_player.assigned_animation == "dissolving"
-		
+
 	animation_player.current_animation = "dissolving"
 	animation_player.pause()
 	var advance_to_pos = (animation_player.current_animation_length - (vol * (animation_player.current_animation_length / max_vol))) - animation_player.current_animation_position
--- loot/loot.gd
+++ loot/loot.gd
@@ -12,3 +12,19 @@ signal volume_changed(vol, max_vol)
 
 
 @export var item_name := "untitled_item"
+
+var image_sprite: Texture2D:
+	get:
+		return $Sprite2D.texture
+
+var image_region_rect:
+	get:
+		return $Sprite2D.region_rect
+
+var image_region_scale:
+	get:
+		return $Sprite2D.scale
+
+var image_size:
+	get:
+		return Vector2($Sprite2D.region_rect.size.x, $Sprite2D.region_rect.size.y)
